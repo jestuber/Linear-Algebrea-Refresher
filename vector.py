e4b06bb02ec2417acc81c96ca90d1ada8b43e796
jestuber <jestuber@gmail.com>
2017-04-05 18:27:24 -0700

    Inner Products and angles
    Added functions for dot product and finding angles

--- vector.py
+++ vector.py
@@ -1,3 +1,4 @@
+import math
 class Vector(object):
     def __init__(self, coordinates):
         try:
@@ -30,6 +31,27 @@ class Vector(object):
         new_coord = [c * x for x in self.coordinates]
         return Vector(new_coord)
 
+    def magnitude(self):
+        return sum([x**2 for x in self.coordinates])**0.5
+
+    def norm(self):
+        try:
+            magnitude = self.magnitude()
+            return self.times_scalar(1. / magnitude)
+        except ZeroDivisionError:
+            raise Exception("Zero Vector has no norm")
+
+    def dot_product(self, v):
+        return sum([x * y for x, y in zip(self.coordinates, v.coordinates)])
+
+    def angle(self, v):
+        try:
+            norm_self = self.norm()
+            norm_v = v.norm()
+            return math.acos(norm_self.dot_product(norm_v))
+        except ZeroDivisionError:
+            raise Exception("Cannot find Angle with Zero Vector")
+
 
 one = Vector([8.218, -9.341])
 print one.plus(Vector([-1.129, 2.111]))
@@ -39,3 +61,13 @@ print two.minus(Vector([-8.223, 0.878]))
 
 three = Vector([1.671, -1.012, -0.318])
 print three.times_scalar(7.41)
+
+print Vector([-0.221, 7.437]).magnitude()
+print Vector([8.813, -1.331, -6.247]).magnitude()
+print Vector([1.996, 3.108, -4.554]).norm()
+print Vector([5.581, -2.136]).norm()
+
+print Vector([7.887, 4.138]).dot_product(Vector([-8.802, 6.776]))
+print Vector([-5.955, -4.904, -1.874]).dot_product(Vector([-4.496, -8.755, 7.103]))
+print Vector([3.183, -7.627]).angle(Vector([-2.668, 5.319]))
+print Vector([7.35, 0.221, 5.188]).angle(Vector([2.751, 8.259, 3.985]))
